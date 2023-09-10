6f696e8b5da12cb1d5d46575d37f7eff866a2acf
rnfor-pro <nfor.rudolph1@gmail.com>
2023-09-10 15:44:15 -0400

    my first commit

---
+++ script.sh
@@ -0,0 +1,6 @@
+echo -n "Please enter your name: "
+read name
+echo -n "Please enter your team: "
+read team
+echo "OHH Nice,Welcome to Etech Devops master class $name and you are also welcome to $team"
+echo "End of script"
