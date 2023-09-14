561d6c3e44c2e2b6ee469da710b60d9bc7fd2100
Etech consulting <99599657+etechDevops@users.noreply.github.com>
2023-09-14 19:05:34 -0400

    Update script.sh
    updated for demo

--- script.sh
+++ script.sh
@@ -4,3 +4,4 @@ echo -n "Please enter your team: "
 read team
 echo "OHH Nice,Welcome to Etech Devops master class $name and you are also welcome to $team"
 echo "End of script"
+#this is just for demo
