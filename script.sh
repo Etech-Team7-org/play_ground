50c960001b55265faeb6eade8b14949323cc21b7
rnfor-pro <nfor.rudolph1@gmail.com>
2023-09-16 12:22:56 -0400

    some comments deleted

--- script.sh
+++ script.sh
@@ -4,6 +4,3 @@ echo -n "Please enter your team: "
 read team
 echo "OHH Nice,Welcome to Etech Devops master class $name and you are also welcome to $team"
 echo "End of script"
-#this is just for demo
-#yet another update
-#thank you for choosing Etech
